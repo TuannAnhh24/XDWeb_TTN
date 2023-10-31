00ffb9961b86e422949982f17f00a0edeaaf0812
tuan2511-nguyen <112092147+tuan2511-nguyen@users.noreply.github.com>
2023-10-31 23:38:53 +0700

    Delete Thithu directory

--- Thithu/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require 'connect.php';
-    $id = $_GET['id'];
-    $sql = "DELETE FROM `courses` WHERE `id` = '$id'";
-    $connect -> exec($sql);
-    header('location:list.php');
-?>  
--- Thithu/function.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function showArray($array){
-        echo "<pre>";
-        print_r($array);
-        echo "</pre>";
-    }
-?>
