454dafe41573d7f9dae809273822e7a106b34b37
ductien2k3 <phamductiendz@gmail.com>
2023-11-09 14:49:39 +0700

    cắt giao diện trang chủ

---
+++ XDWeb_TTN/view/home.php
@@ -0,0 +1,29 @@
+<main>
+        <section class="courses">
+            <h2>Chuyên đề</h2>
+            <div class="course">
+                <a href="toan.html"><img src="../XDWeb_TTN/anh/Toan-cap-2.jpg"></a>
+                <div class="description">
+                    <a href="toan.html"><h3 class="chuyende">TOÁN</h3></a>
+                    <p class="mota">1 + 1 = anh yêu em!!! <br> Started - Oct 30, 2023</p>
+                </div>
+                <a href="toan.html" class="btn-xem"><button class="right-button">Xem</button></a>
+            </div>
+            <div class="course">
+                <a href="tienganh.html"><img src="../XDWeb_TTN/anh/tieng-anh.jpg" ></a>
+                <div class="description">
+                    <a href="tienganh.html"><h3 class="chuyende">TIẾNG ANH </h3></a>
+                    <p class="mota">WTF What are you doing?? <br> Started - Oct 30, 2023</p>
+                </div>
+                <a href="tienganh.html" class="btn-xem"><button class="right-button">Xem</button></a>
+            </div>
+            <div class="course">
+                <a href="vatly.html"><img src="../XDWeb_TTN/anh/vatly.jpg" ></a>
+                <div class="description">
+                    <a href="vatly.html"><h3 class="chuyende">VẬT LÝ </h3></a>
+                    <p class="mota">Vàng có dẫn tiền không??? <br> Started - Oct 30, 2023</p>
+                </div>
+                <a href="vatly.html" class="btn-xem"><button class="right-button">Xem</button></a>
+            </div>
+        </section>
+    </main>
