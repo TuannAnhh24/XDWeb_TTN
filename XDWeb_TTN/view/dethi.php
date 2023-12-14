70f54e92720810365d39f93ccf347552d2c7279f
ductien2k3 <phamductiendz@gmail.com>
2023-12-14 19:20:08 +0700

    á á

--- XDWeb_TTN/view/dethi.php
+++ XDWeb_TTN/view/dethi.php
@@ -5,9 +5,15 @@
             <h2>Đề Thi</h2>
             <?php
             $tenkythi = '';
+            $batdau = '';
+            $ketthuc = '';
+            $thoigianthi = '';
                 foreach ($list_lichthi as $lt) {
                 if ($lt['id'] == $id_lichthi) {
                     $tenkythi =  $lt['tenkythi'];
+                    $batdau =  $lt['batdau'];
+                    $ketthuc =  $lt['ketthuc'];
+                    $thoigianthi =  $lt['thoigianthi'];
                     break;
                 }
                 }
@@ -20,6 +26,7 @@
                         <h3 class="chuyende">'.$ten_dethi.'</h3>
                     </a>
                     <p class="mota">số câu : '.$socau.' <br> Tên Kì Thi - '.$tenkythi.'</p>
+                    <p class="mota">Bắt đầu '.$batdau.' -  Kết Thúc '.$ketthuc.' Thời Gian thi : '.$thoigianthi.'</p>
                 </div>
                 <a href="toan.html" class="btn-xem"><button class="right-button">Vào Thi</button></a>
             </div>';
